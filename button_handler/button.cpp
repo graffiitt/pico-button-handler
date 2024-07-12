84aefb8e36deb68cc85dbac0ec63a40493eea3d9
vlad mac <vladikkopylov31@gmail.com>
2024-07-12 22:49:02 +0300

    [REF] add struct for description button

--- button_handler/button.cpp
+++ button_handler/button.cpp
@@ -1,17 +1,73 @@
 #include "button.h"
 
+struct Button button[4];
+
+bool lastState[] = {1, 1, 1, 1};
+int32_t timePress[4];
+
+void settingButton(const Button *bt)
+{
+    gpio_init(bt->numberPin);
+    gpio_set_dir(bt->numberPin, GPIO_IN);
+    gpio_pull_up(bt->numberPin);
+}
+
+void handlerButton(bool state, Button *bt)
+{
+    if (!state && bt->lastState) // fix pressed
+    {
+        bt->flag = 1;
+        printf("pressed %d %d \n", bt->numberPin, time_us_32());
+        bt->timePress = time_us_32();
+    }
+
+    uint32_t deltaTime = time_us_32() - bt->timePress;
+
+    if (!(state && bt->lastState) &&
+        (deltaTime > LONG_PRESS_TIME) && bt->flag)
+    {
+        bt->flag = 0;
+        printf("pressed long %d %d \n", bt->numberPin, deltaTime);
+    }
+    if ((state && !bt->lastState) && (deltaTime < LONG_PRESS_TIME))
+    {
+
+        printf("pressed short %d %d \n", bt->numberPin, deltaTime);
+    }
+    bt->lastState = state;
+}
+
 void alarmIrq()
 {
+    // set timer for next irq
     hw_clear_bits(&timer_hw->intr, 1u << ALARM_NUM);
     printf("Alarm IRQ  pin state: %d\n", gpio_get(6));
     timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + TIMER_DELAY;
+
+    bool state[] = {
+        gpio_get(BUTTON_1),
+        gpio_get(BUTTON_2),
+        gpio_get(BUTTON_3),
+        gpio_get(BUTTON_4),
+    };
+
+    if (3 > (state[0] + state[1] + state[2] + state[3]))
+        return;
+
+    handlerButton(state[0], &button[0]);
 }
 
 void buttonHandlerInit()
 {
-    gpio_init(6);
-    gpio_set_dir(6, GPIO_IN);
-    gpio_pull_up(6);
+    button[0].numberPin = BUTTON_1;
+    button[1].numberPin = BUTTON_2;
+    button[2].numberPin = BUTTON_3;
+    button[3].numberPin = BUTTON_4;
+
+    for (int i = 0; i < 4; i++)
+    {
+        settingButton(&button[i]);
+    }
 
     hw_set_bits(&timer_hw->inte, 1u << ALARM_NUM);
     irq_set_exclusive_handler(ALARM_IRQ, alarmIrq);
--- button_handler/button.h
+++ button_handler/button.h
@@ -7,10 +7,26 @@
 #include "hardware/timer.h"
 #include "hardware/irq.h"
 
-#define TIMER_DELAY 1000000
+#define BUTTON_1 6
+#define BUTTON_2 7
+#define BUTTON_3 8
+#define BUTTON_4 9
+
+#define LONG_PRESS_TIME 3000000 // 3 sec
+#define TIMER_DELAY 250000  // 250 ms
 #define ALARM_NUM 0
 #define ALARM_IRQ TIMER_IRQ_0
 
+struct Button
+{
+    bool flag;
+    int numberPin;
+    bool lastState;
+    uint32_t timePress; // timestamp when button was pressed
+};
+
+void settingButton(const Button *bt);
+void handlerButton(bool state, Button *bt);
 void alarmIrq();
 void buttonHandlerInit();
 
